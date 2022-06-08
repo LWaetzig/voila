0fe6b5004d7026f9f51527976df48c290aa49b27
LWaetzig <lucas@waetzig.net>
2022-06-08 20:53:21 +0200

    feat(Stock.py): create class for stock and prediction

---
+++ src/Stock.py
@@ -0,0 +1,259 @@
+import datetime as dt
+import warnings
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import plotly.graph_objs as go
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+
+import warnings
+
+warnings.simplefilter("ignore")
+
+
+
+
+class Stock():
+    """Class to load and clean up stock data from yahoo finance 
+    """
+
+    def __init__(self , name : str, period : str):
+        self.name = name
+        self.period = period
+
+    def get_stock_data(self) -> pd.DataFrame:
+        """get stock data for a specific company downloaded using yahoo finance
+
+        Args:
+            name (str): international stock name (e.g. AAPL -> Apple Inc.)
+            period (str): time interval for stock data (mo:= Months , y:=years , wk:=weeks, d:=days)
+
+        Returns:
+            data (pd.DataFrame): pd.DataFrame containing stock data from yahoo finance
+        """
+
+        if self.period == "1d":
+            interval="1h"
+        else:
+            interval="1d"
+        
+        data = yf.Ticker(self.name).history(self.period , interval)
+        data = data[["Open" , "Close" , "High" , "Low"]]
+
+        # add mean value of stock price per day
+        data["Mean"] = data.mean(axis="columns")
+
+        data["mva50"] = data["Open"].rolling(50).mean()
+        data["mva200"] = data["Open"].rolling(200).mean()
+
+        data["date_value"] = data.index.map(dt.datetime.toordinal)
+
+        return data
+
+
+    def create_future_dates(self , df) -> pd.DataFrame:
+        """function to create future dates as datetime objects depending on passed days
+
+        Args:
+            df (pd.DataFrame): dataframe with future dates
+
+        Returns:
+            pd.DataFrame: dataframe with future dates
+        """
+
+        predicted_days = 7
+
+        # create columns for days, months, years from datetime index
+        df["day"] = df.index.day
+        df["month"] = df.index.month
+        df["year"] = df.index.year
+
+        df = df[["day" , "month" , "year"]]
+
+        # drop column with old datetime objects
+        df = df.reset_index(drop=True)
+
+        # get latest date in DataFrame
+        last_date = [list(df.iloc[-1])]
+        future_dates = list()
+
+        # append dataframe with future dates depending on input
+        for i in range(predicted_days + 1):
+            appended = [(last_date[0][0] + i), last_date[0][1], last_date[0][2]]
+            if appended[0] > 31 and appended[1] > 12:
+                appended[0] = 1
+                appended[1] = 1
+                appended[2] = appended[2] + 1
+
+            if (
+                appended[1] == 1
+                or appended[1] == 3
+                or appended[1] == 5
+                or appended[1] == 7
+                or appended[1] == 8
+                or appended[1] == 10
+                or appended[1] == 12
+            ):
+                if appended[0] > 31:
+                    appended[0] = appended[0] - 31
+                    appended[1] = appended[1] + 1
+
+            elif appended[1] == 2:
+                if appended[0] > 28:
+                    appended[0] = appended[0] - 28
+                    appended[1] = appended[1] + 1
+
+            else:
+                if appended[0] > 30:
+                    appended[0] = appended[0] - 28
+                    appended[1] = appended[1] + 1
+
+            future_dates.append(appended)
+
+        #create new dataframe for future dates
+        future_dates_df = pd.DataFrame(columns=["day", "month", "year"])
+
+        # append new dataframe with new dates
+        for liste in future_dates:
+            series = pd.Series(liste, index=df.columns)
+            future_dates_df = future_dates_df.append(series, ignore_index=True)
+        
+        # create datetime values from strings
+        for i in range(len(future_dates_df)):
+            day = str(future_dates_df.loc[i, "day"])
+            month = str(future_dates_df.loc[i, "month"])
+            year = str(future_dates_df.loc[i, "year"])
+
+            date_string = f"{day}/{month}/{year}"
+            future_dates_df.loc[i, "Date"] = dt.datetime.strptime(date_string, "%d/%m/%Y")
+
+        future_dates_df = future_dates_df[["Date"]]
+        # create ordinal numbers for datetime objects
+        future_dates_df["date_value"] = future_dates_df["Date"].map(dt.datetime.toordinal)
+        future_dates_df = future_dates_df.set_index(future_dates_df["Date"])
+        future_dates_df = future_dates_df.drop(columns=["Date"])
+    
+        return future_dates_df
+
+
+    def visualize_data(self , df : pd.DataFrame) -> None:
+        """vizualisze stock data using plotly
+
+        Args:
+            df (pd.DataFrame): _description_
+        """
+        
+        stocks = dict()
+        stocks["ACN"] = "Accentur"
+        stocks["SIE.DE"] = "Siemens AG"
+        stocks["GME"] = "Gamestop"
+        stocks["DOGE-USD"] = "Dogecoin :D"
+        stocks["BTC-USD"] = "Bitcoin"
+        stocks["DAX"] = "DAX PERFORMANCE-INDEX"
+        stocks["AAPL"] = "Apple Inc."
+        stocks["TSLA"] = "Tesla Inc."
+        stocks["AMZN"] = "Amazon.com, Inc."
+        stocks["TWTR"] = "Twitter Inc"
+
+        fig = go.Figure()
+
+        fig.add_scatter(
+            x=df.index,
+            y=df["Mean"],
+            name = "mean price",
+            opacity=0.7
+        )
+
+        fig.add_scatter(
+            x=df.index,
+            y=df["mva50"],
+            name="moving average 50 days",
+            opacity=0.5
+        )
+
+        fig.add_scatter(
+            x=df.index,
+            y=df["mva200"],
+            name="moving average 200 days",
+            opacity=0.5
+        )
+
+        fig.add_trace(
+            go.Candlestick(
+                x=df.index,
+                open=df["Open"],
+                high=df["High"],
+                low=df["Low"],
+                close=df["Close"],
+                name="stock price"
+            )
+        )
+
+        fig.update_layout(
+            title=f"Stock for {stocks[self.name]}",
+            yaxis_title="Stock price USD ($)",
+            xaxis_title="Date"
+        )
+
+        fig.show()
+
+
+
+class Predictor(Stock):
+    def __init__(self, model , deg=None):
+        self.model = model
+        self.deg = deg if deg is not None else None
+
+
+
+    def make_prediction(self , df) -> pd.DataFrame:
+        """function to predict data using passed model
+
+        Returns:
+            pred_df (pd.DataFrame): pd.DataFrame with predicted values
+        """
+
+        if self.model == "linreg":
+            pred_model = np.poly1d(np.polyfit(df["date_value"], df["Close"], deg=self.deg))
+
+            super().__init__
+            future_dates_df = Stock.create_future_dates(df)
+
+            pred_df = pd.DataFrame(columns="predicted")
+            pred_df["predicted"] = pred_model(future_dates_df["date_values"])
+
+        return pred_df
+
+
+    def trendline(self) -> pd.DataFrame:
+        pass
+        return pd.DataFrame
+
+    def evaluate_model(self) -> None:
+        """function to evaluate model using r2_score, mae, rmse
+
+        Args:
+            df (pd.DataFrame): dataset on which values were predicted
+            model (_type_): prediction model used
+        """
+
+        df = df[["Close"]]
+
+        # store ordinal values of datetime objects in X and stock values in y
+        X = df.index.map(dt.datetime.toordinal)
+        y = df["Close"]
+
+        # split dataset into train and test batch
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    
+
+    def __str__(self):
+        return 
+
+
+
+    
