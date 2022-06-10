f7d8f697642d6ed2b8ea512bc1926e5677ffec73
LWaetzig <lucas@waetzig.net>
2022-06-11 00:02:49 +0200

    feat(stocks.py): restructure code

--- src/stocks.py
+++ src/stocks.py
@@ -1,177 +1,96 @@
 import datetime as dt
-import warnings
-
-import numpy as np
-import pandas as pd
 import yfinance as yf
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Lasso
+import pandas as pd
+import plotly.graph_objs as go
 
-from Stock import Stock, Predictor
+from Prediction import Predictor
 
-Stock = Stock("AAPL" , "5y")
-df = Stock.get_stock_data()
+period = "7w"
 
-Stock.visualize_data(df)
-
-Predictor = Predictor(model="linreg" , deg=3)
-predicted_df = Predictor.make_prediction(df)
-
-warnings.simplefilter("ignore")
+if "d" in period:
+    print(True)
 
 
-def create_future_dates(df, days=None, weeks=None, months=None) -> pd.DataFrame:
-    """function to create future dates as datetime objects depending on passed days
+def get_stock_data(stock_name : str , period : str) -> pd.DataFrame:
+    """get stock data for a specific company downloaded using yahoo finance
 
     Args:
-        df (pd.DataFrame): dataframe with datetime as index
-        days (int , optional): number of days to be predicted. Default is None
-        weeks (int , optional): number of weeks to be predicted. Default is None
-        months (int , optional): number of months to be predicted. Default is None
+        name (str): international stock name (e.g. AAPL -> Apple Inc.)
+        period (str): time interval for stock data (mo:= Months , y:=years , wk:=weeks, d:=days)
 
     Returns:
-        pd.DataFrame: dataframe with future dates
+        data (pd.DataFrame): pd.DataFrame containing stock data from yahoo finance
     """
 
-    # set predicted_days depending on input
-    if days:
-        predicted_days = days
-    elif weeks:
-        predicted_days = weeks * 7
-    elif months:
-        predicted_days = months * 31
+    if "d" in period:
+        interval = "1m"
+    else:
+        interval = "1d"
 
-    # create columns for days, months, years from datetime index
-    df["day"] = df.index.day
-    df["month"] = df.index.month
-    df["year"] = df.index.year
-    df = df.reset_index()
+    data = yf.Ticker(stock_name).history(period, interval)
+    data = data[["Open", "Close", "High", "Low"]]
 
-    # slice DataFrame to keep only columns "day" , "month" , "year"
-    date_df = df[["day", "month", "year"]]
+    # add mean value of stock price per day
+    data["Mean"] = data.mean(axis="columns")
 
-    # get latest date in DataFrame
-    last_date = [list(date_df.iloc[-1])]
-    future_dates = list()
+    if "d" not in period or "w" not in period:
+        data["mva"] = data["Open"].rolling(50).mean()
 
-    # append dataframe with future dates depending on input
-    for i in range(predicted_days + 1):
-        appended = [(last_date[0][0] + i), last_date[0][1], last_date[0][2]]
-        if appended[0] > 31 and appended[1] > 12:
-            appended[0] = 1
-            appended[1] = 1
-            appended[2] = appended[2] + 1
+    data["date_value"] = data.index.map(dt.datetime.toordinal)
 
-        if (
-            appended[1] == 1
-            or appended[1] == 3
-            or appended[1] == 5
-            or appended[1] == 7
-            or appended[1] == 8
-            or appended[1] == 10
-            or appended[1] == 12
-        ):
-            if appended[0] > 31:
-                appended[0] = appended[0] - 31
-                appended[1] = appended[1] + 1
+    return data
 
-        elif appended[1] == 2:
-            if appended[0] > 28:
-                appended[0] = appended[0] - 28
-                appended[1] = appended[1] + 1
+df = get_stock_data("AAPL" , "1d")
 
-        else:
-            if appended[0] > 30:
-                appended[0] = appended[0] - 28
-                appended[1] = appended[1] + 1
-
-        future_dates.append(appended)
-
-    # create new dataframe for future dates
-    future_dates_df = pd.DataFrame(columns=["day", "month", "year"])
-
-    # append new dataframe with new dates
-    for liste in future_dates:
-        series = pd.Series(liste, index=date_df.columns)
-        future_dates_df = future_dates_df.append(series, ignore_index=True)
-
-    # create datetime values
-    for i in range(len(future_dates_df)):
-        day = str(future_dates_df.loc[i, "day"])
-        month = str(future_dates_df.loc[i, "month"])
-        year = str(future_dates_df.loc[i, "year"])
-
-        date_string = f"{day}/{month}/{year}"
-        future_dates_df.loc[i, "Date"] = dt.datetime.strptime(date_string, "%d/%m/%Y")
-
-    future_dates_df = future_dates_df[["Date"]]
-    # create ordinal numbers for datetime objects
-    future_dates_df["date_value"] = future_dates_df["Date"].map(dt.datetime.toordinal)
-    future_dates_df = future_dates_df.set_index(future_dates_df["Date"])
-    future_dates_df = future_dates_df.drop(columns=["Date"])
-
-    return future_dates_df
-
-
-def evaluate_model(df, model = None , deg = None) -> None:
-    """function to evaluate model using r2_score, mae, rmse
+def visualize_data(df: pd.DataFrame , stock_name : str, **add_args) -> None:
+    """vizualisze stock data using plotly
 
     Args:
-        data (pd.DataFrame): original stock data
-        deg (int): degree of regression
+        df (pd.DataFrame): _description_
     """
 
-    # only keep "Open" column in DataFrame
-    df = df[["Close"]]
+    stocks = dict()
+    stocks["ACN"] = "Accentur"
+    stocks["SIE.DE"] = "Siemens AG"
+    stocks["GME"] = "Gamestop"
+    stocks["DOGE-USD"] = "Dogecoin :D"
+    stocks["BTC-USD"] = "Bitcoin"
+    stocks["DAX"] = "DAX PERFORMANCE-INDEX"
+    stocks["AAPL"] = "Apple Inc."
+    stocks["TSLA"] = "Tesla Inc."
+    stocks["AMZN"] = "Amazon.com, Inc."
+    stocks["TWTR"] = "Twitter Inc"
 
-    # store ordinal values of datetime objects in X and stock values in y
-    X = df.index.map(dt.datetime.toordinal)
-    y = df["Close"]
+    fig = go.Figure()
 
-    # split dataset into train and test batch
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    fig.add_scatter(x=df.index, y=df["Mean"], name="mean price", opacity=0.7)
 
-    if model == None:
-        # create model using np regresseion of n-degree
-        model = np.poly1d(np.polyfit(X_train, y_train, deg=deg))
+    fig.add_scatter(
+        x=df.index, y=df["mva"], name="moving average", opacity=0.5
+    )
 
-    else:
-        X_train = np.array(X_train).reshape(-1,1)
-        y_train = np.array(y_train).reshape(-1,1)
+    fig.add_trace(
+        go.Candlestick(
+            x=df.index,
+            open=df["Open"],
+            high=df["High"],
+            low=df["Low"],
+            close=df["Close"],
+            name="stock price",
+        )
+    )
 
-        model.fit(X_train , y_train)
-    
-    # use model and predict values
-    y_pred = model(X_test)
+    fig.update_layout(
+        title=f"Stock for {stocks[stock_name]}",
+        yaxis_title="Stock price USD ($)",
+        xaxis_title="Date",
+    )
 
-    # print out metrics for predicted data and evaluate model
-    print("R^2 : ", r2_score(y_test, y_pred))
-    print("MAE : ", mean_absolute_error(y_test, y_pred))
-    print("RMSE : ", np.sqrt(mean_squared_error(y_test, y_pred)))
+    fig.show()
 
-
-# get data from api for 5 years back
-data = yf.Ticker("AAPL").history("5y")
-
-# clean dataframe from unnecessary columns
-data = data.drop(columns=["Volume", "Dividends", "Stock Splits"])
-data = data[["Close"]]
-data["date_value"] = data.index.map(dt.datetime.toordinal)
-
-# create prediciton model by using np regression
-pred_model = np.poly1d(np.polyfit(data["date_value"], data["Close"], deg=3))
-
-df = create_future_dates(data, months=1)
-
-df["predicted"] = pred_model(df["date_value"])
-df = df.drop(columns=["date_value"])
-
-evaluate_model(data, model = "poly" , deg=3)
-
-data = data.drop(columns=["date_value"])
+visualize_data(df , "AAPL")
 
 
-lasso_model = Lasso(alpha=1)
-
-evaluate_model(data , lasso_model)
+predictor = Predictor(model="linreg", deg=3)
+predictor.evaluate_model(df)
+predicted_df = predictor.make_prediction(df)
